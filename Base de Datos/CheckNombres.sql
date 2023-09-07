2a3d9799c33e405257815e41d4f0da7761a3259f
Ciarango10 <carlosarango3101@gmail.com>
2023-09-07 16:04:25 -0500

    Nombres

--- Base de Datos/CheckNombres.sql
+++ Base de Datos/CheckNombres.sql
@@ -1,2 +1,3 @@
 ALTER TABLE Alumno
-ADD CONSTRAINT CHK_Alumno CHECK(Nombre NOT LIKE '%[^0-9-A-Z-az]%')
+ADD CONSTRAINT CHK_Alumno CHECK(Nombre NOT LIKE '%[^A-Z-az]%') --Incluye
+ADD CONSTRAINT CHK_Alumno CHECK(Nombre LIKE '%[^0-9]%') --Excluye
