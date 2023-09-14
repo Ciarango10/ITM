bcab84484403c03bcccbfc56128c25e6db02e289
Ciarango10 <carlosarango3101@gmail.com>
2023-09-13 21:08:39 -0500

    Cambios

--- Base de Datos/Check.sql
+++ Base de Datos/Check.sql
@@ -1,3 +1,7 @@
 ALTER TABLE Clientes
 ADD CONSTRAINT CK_Clientes_Documento
 CHECK (Documento > 0);
+
+ALTER TABLE Alumno
+ADD CONSTRAINT CHK_Alumno_Incluye CHECK (Nombre NOT LIKE '%[^A-Za-z]%'), -- Incluye solo letras
+ADD CONSTRAINT CHK_Alumno_Excluye CHECK (Nombre LIKE '%[^0-9]%'); -- Excluye números
--- Base de Datos/CheckNombres.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Alumno
-ADD CONSTRAINT CHK_Alumno CHECK(Nombre NOT LIKE '%[^A-Z-az]%') --Incluye
-ADD CONSTRAINT CHK_Alumno CHECK(Nombre LIKE '%[^0-9]%') --Excluye
