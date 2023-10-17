caab955dedec49b221f7f99c28036c636e159b43
Ciarango10 <carlosarango3101@gmail.com>
2023-10-17 10:27:17 -0500

    Cambios en funciones

--- Base de Datos/FuncionesConversion.sql
+++ Base de Datos/FuncionesConversion.sql
@@ -1,9 +1,9 @@
--- TRANSFORMA UNO O MAS VALORES DE TIPO DE DATOS A OTRO.
---CAST()
+-- Transforma uno o mas valores de tipo de datos a otro.
+-- CAST()
 SELECT 'El codigo ' + CAST(IdCancion AS VARCHAR(5))
 + ' corresponde a la cancion ' + Titulo + ' y tiene una duracion de ' + CAST(Milisegundos as VARCHAR(7))
 FROM Cancion;
---CONVERT()
+-- CONVERT()
 SELECT 'El Album con codigo ' + CONVERT(VARCHAR(5),IdAlbum) + ' y titulo ' + Titulo +
 ' fue lanzado al publico el ' + CONVERT(VARCHAR(10),Lanzamiento)
 FROM Album;
--- Base de Datos/TallerBD.sql
+++ Base de Datos/TallerBD.sql
@@ -1,4 +1,4 @@
---TALLER 
+-- TALLER 
 -- 1)
 SELECT p.PeliculaTitulo FROM Pelicula p 
 INNER JOIN Director d ON d.DirectorCodigo = p.PeliculaDirector
@@ -18,24 +18,24 @@ SELECT r.* FROM Pelicula p
 INNER JOIN Reparto r ON r.PeliculaCodigo = p.PeliculaCodigo
 WHERE p.PeliculaTitulo = 'Casa Blanca';
 
---5) 
+-- 5) 
 SELECT p.PeliculaTitulo FROM Pelicula p 
 INNER JOIN Reparto r ON r.PeliculaCodigo = p.PeliculaCodigo
 INNER JOIN Actor a ON a.ActorCodigo = r.PeliculaActor
 WHERE a.ActorNombre = 'Will Smith';
 
---6)
+-- 6)
 SELECT PeliculaTitulo FROM Pelicula 
 WHERE PeliculaFecha LIKE '2019-12-%';
 
---7) 
+-- 7) 
 SELECT PeliculaTitulo FROM Pelicula p
 INNER JOIN Ejemplar e ON e.PeliculaCodigo = p.PeliculaCodigo
 INNER JOIN DetalleAlquiler da ON da.EjemplarCodigo = e.EjemplarCodigo
 INNER JOIN Alquiler a ON a.NumeroAlquiler = da.NumeroAlquiler
 WHERE a.AlquilerFecha LIKE '2020-03-%';
 
---8)
+-- 8)
 SELECT TOP 5 COUNT(e.EjemplarCodigo) FROM Pelicula p
 INNER JOIN Ejemplar e ON e.PeliculaCodigo = p.PeliculaCodigo;
 
