b11b66901bfd67c8d79528f77ff3d8fc159c7d68
Ciarango10 <118235923+Ciarango10@users.noreply.github.com>
2023-10-27 17:58:12 -0500

    Update FuncionesNULL.sql

--- Base de Datos/FuncionesNULL.sql
+++ Base de Datos/FuncionesNULL.sql
@@ -2,11 +2,13 @@
 -- Si el valor del primer argumento es nulo, se le asignará el valor del segundo argumento al campo
 SELECT IdProyecto, Titulo, ISNULL(Presupuesto,0) AS Presupuesto
 FROM Proyecto;
+
 -- NULLIF()
 -- Si los argumentos de la funcion tienen el mismo valor retorna NULL, de lo contrario
 -- retorna el valor del primer argumento
 SELECT NULLIF(Presupuesto,0) AS PresupuestoXProyecto
 FROM Proyecto;
+
 -- COALESCE()
 -- Si el valor del primer argumento es nulo, se le asignará el valor del segundo argumento al campo
 SELECT Contacto, COALESCE(Nombre,'Se desconoce') AS Nombre
