342b436c7b269d96ea54ce6ad1ecb0a13e027ed4
Ciarango10 <carlosarango3101@gmail.com>
2023-11-04 12:43:39 -0500

    Cambios ProcedimientosAlmacenados

--- Base de Datos/ProcedimientosAlmacenados.sql
+++ Base de Datos/ProcedimientosAlmacenados.sql
@@ -3,9 +3,8 @@ CREATE PROCEDURE sp_Cancion
 AS
 	SELECT * FROM Cancion
 	WHERE Titulo LIKE @Titulo+'%';
-GO
 EXECUTE sp_Cancion 'O';
-GO
+
 CREATE PROCEDURE sp_Insertar_Artista
 	@IdArtista INT,
 	@Nombre VARCHAR(25),
@@ -14,9 +13,8 @@ AS
 BEGIN
 	INSERT INTO Artista (IdArtista, Nombre,Contacto) VALUES (@IdArtista,@Nombre, @Contacto);
 END
-GO
 EXECUTE sp_Insertar_Artista 14, 'Adele', 'adele@business.com';
-GO
+
 CREATE PROCEDURE sp_Editar_Productor
 	@IdProductor INT,
 	@Nombre VARCHAR(25),
@@ -26,5 +24,4 @@ AS
 	SET Nombre = @Nombre,
 	Contacto = @Contacto
 	WHERE IdProductor = @IdProductor;
-GO
 EXECUTE sp_Editar_Productor 1, 'SOGX', 'sog@business.com';
