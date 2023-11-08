78be7c57eb3b09c41d405debf1e559da3fc51f45
Ciarango10 <carlosarango3101@gmail.com>
2023-11-07 19:00:58 -0500

    Triggers

--- Base de Datos/EntregaFinal/QueryPracticaFinal.sql
+++ Base de Datos/EntregaFinal/QueryPracticaFinal.sql
@@ -605,3 +605,44 @@ GO
 EXECUTE sp_Editar_Productor 10, 'Tainy', 'tainy@example.com';
 GO
 -- TRIGGERS
+CREATE TABLE HistorialArtista
+(CodHistorialArtista int identity(1,1) primary key,
+Fecha date,
+Accion varchar(100),
+Usuario varchar(100)
+)
+GO
+CREATE TRIGGER Tr_Insert_Artista
+ON Artista AFTER INSERT 
+AS 
+BEGIN 
+	INSERT INTO HistorialArtista (Fecha, Accion, Usuario)
+	VALUES (GETDATE(), 'Se agregó un Artista', USER);
+END
+GO
+INSERT INTO Artista (IdArtista, Nombre, Contacto) 
+VALUES (16, 'Eladio Carrion', 'eladio@example.com');
+GO
+CREATE TRIGGER Tr_Delete_Artista
+ON Artista FOR DELETE 
+AS 
+BEGIN 
+	INSERT INTO HistorialArtista (Fecha, Accion, Usuario)
+	VALUES (GETDATE(), 'Se eliminó un Artista', USER);
+END
+GO
+DELETE FROM Artista WHERE IdArtista = 16;
+GO
+CREATE TRIGGER Tr_Update_Artista
+ON Artista AFTER UPDATE
+AS 
+BEGIN 
+	INSERT INTO HistorialArtista (Fecha, Accion, Usuario)
+	VALUES (GETDATE(), 'Se actualizó un Artista', USER);
+END
+GO
+UPDATE Artista
+SET Nombre = 'Bruno Marte' 
+WHERE IdArtista = 15;
+GO
+
