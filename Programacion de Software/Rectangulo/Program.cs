4a3f23e86a2203fa21b681ccf989cec1c3c970a2
Ciarango10 <carlosarango3101@gmail.com>
2024-02-25 16:57:14 -0500

    Programacion de software

---
+++ Programacion de Software/Rectangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Calcular el area y perimetro de un rectangulo
+
+class Program
+{
+    static void Main()
+    {
+        float Ancho, Altura, Area, Perimetro;
+
+        Console.Write("Ingrese el ancho del rectangulo: ");
+        Ancho = float.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la altura del rectangulo: ");
+        Altura = float.Parse(Console.ReadLine());
+
+        Area = Ancho * Altura;
+        Perimetro = (Ancho * 2) + (Altura * 2);
+        Console.WriteLine("El area es: " + Area);
+        Console.WriteLine("El Perimetro es: " + Perimetro);
+    }
+}
